57bb4e2d11cbb32bc493c2eb0eae9ac0464b1f25
wandesky <bwandesky@gmail.com>
2019-07-09 15:27:09 +0300

    make components/squares interactive

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null
+    };
+  }
   render() {
     return (
-      <button className="square" onClick={() => alert('click')}>
-        {this.props.value}
+      <button className="square" 
+      onClick={() => this.setState({value: "X"})}>
+        {this.state.value}
       </button>
     );
   }
@@ -18,7 +25,7 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = "Next player: X";
 
     return (
       <div>
@@ -61,7 +68,4 @@ class Game extends React.Component {
 
 // ========================================
 
-ReactDOM.render(
-  <Game />,
-  document.getElementById('root')
-);
+ReactDOM.render(<Game />, document.getElementById("root"));
