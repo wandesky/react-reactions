48fde041019d5f2c491c2cea1de9a9269467e58d
wandesky <bwandesky@gmail.com>
2019-07-12 18:57:49 +0300

    alternate X and O for each move/click

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -31,14 +31,16 @@ class Board extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      squares: Array(9).fill(null)
+      squares: Array(9).fill(null),
+      xIsNext: true,
     };
   }
 
   handleClick(i){
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({squares: squares});
+    xIsNext: !this.state.xIsNext,
   }
   renderSquare(i) {
     return (
